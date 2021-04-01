069ff22ec18c188b0aaee2d6952219cd19c51b54
isenkovska <isenkovska@gmail.com>
2021-03-31 22:42:39 -0400

    image upload TC-114

--- test/extended/image.js
+++ test/extended/image.js
@@ -15,13 +15,12 @@ describe('Image upload suite', function () {
             const fileUpload = $(sel.imageHiddenInput);
             const imgAlt = $(sel.imgAlt);
             const filePath = path.join(__dirname, '../../data/testImages/JPEG 2mb.jpg');
+            const path = require('path');
             browser.execute(
                 (el) => el.style.display = 'block',
                 fileUpload
             );
             fileUpload.waitForDisplayed();
-            const path = require('path');
-
             fileUpload.setValue(filePath);
             expect (imgAlt).toBeExisting();
 
